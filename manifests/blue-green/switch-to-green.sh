5ff606ed6c1833fccdd4886773439c395a183d78
rootpromptnext <rootpromptnext@gmail.com>
2026-03-27 11:12:03 +0000

    update

--- manifests/blue-green/switch-to-green.sh
+++ manifests/blue-green/switch-to-green.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-kubectl patch svc echo -p '{"spec":{"selector":{"app":"echo","version":"green"}}}'
+
+echo "Switching traffic to GREEN..."
+kubectl patch service echo -p '{"spec": {"selector": {"app": "echo", "version": "green"}}}'
+
+echo "Traffic switched to GREEN"
+kubectl get svc echo -o wide
